a83b455bbdd26a4e61751a02387ea261055f706d
liulin14 <liulin14@meituan.com>
2026-03-13 10:52:46 +0800

    【NoCode】编辑页面元素

--- src/components/CareGuideCard.jsx
+++ src/components/CareGuideCard.jsx
@@ -30,7 +30,7 @@ const CareGuideCard = ({ title, icon, items }) => {
             {items.map((item, index) =>
           <li key={index} className="border-b border-gray-100 pb-3 last:border-0 flex justify-center items-center flex-col">
                 <div className="text-gray-800 flex">
-                  <span className="text-body-md break-words text-left w-full pl-0 font-bold">{item.text}</span>
+                  <span className="text-body-md break-words text-left pl-0 font-bold w-[250px]">{item.text}</span>
                 </div>
                 {item.subItems &&
             <ul className="mt-2 ml-0 space-y-1">
